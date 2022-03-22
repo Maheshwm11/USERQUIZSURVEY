9cd331e4fe8e6d87d5ab83cb74c13bc10a08e96b
Madhav Maheshwari <maheshwm@purdue.edu>
2022-03-21 22:21:05 -0400

    Made data.java and quiz.java

--- src/src/Data.java
+++ src/src/Data.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class data {
+import java.io.*;
+import java.util.ArrayList;
+
+public class Data {
+    public ArrayList<Quiz> readInputFile(String directory, String name) {
+        File dir = new File(directory);
+        ArrayList<Quiz> quizzes = new ArrayList<>();
+        File f = new File(dir, name);
+        try (BufferedReader bfr = new BufferedReader(new FileReader(f))) {
+            String line = bfr.readLine();
+            while (line != null) {
+                String[] array = line.split(",");
+                Quiz temp = new Quiz(array[0], array[1], array[2], array[3], array[4], Integer.parseInt(array[5]),
+                        Integer.parseInt(array[6]), Integer.parseInt(array[7]), array[8], false, false);
+                quizzes.add(temp);
+                line = bfr.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return quizzes;
+    }
+
 }
+
--- src/src/Quiz.java
+++ src/src/Quiz.java
@@ -1,18 +1,117 @@
-import java.util.ArrayList;
-
-public class quiz {
-    private String studentName;
+public class Quiz {
+    private String courseName;
     private String quizName;
+    private String studentName;
     private String studentEmail;
-    private ArrayList<String> questions = new ArrayList<>();
-    private boolean randomizationChoice;
+    private String timestamp;
+    private int totalQuestions;
+    private int correctQuestions;
+    private float grade;
+    private String firstQuestion;
+    private boolean isExam;
+    private boolean isTask;
 
-    public quiz(String course, String quizName) {
-
-
+    public Quiz(String courseName, String quizName, String studentName, String studentEmail,
+                String timestamp, int totalQuestions, int correctQuestions, float grade,
+                String firstQuestion, boolean isExam, boolean isTask) {
+        this.courseName = courseName;
+        this.quizName = quizName;
+        this.studentName = studentName;
+        this.studentEmail = studentEmail;
+        this.timestamp = timestamp;
+        this.totalQuestions = totalQuestions;
+        this.correctQuestions = correctQuestions;
+        this.grade = grade;
+        this.firstQuestion = firstQuestion;
+        this.isExam = isExam;
+        this.isTask = isTask;
     }
 
-    public static void main(String[] args) {
+    public String getCourseName() {
+        return courseName;
+    }
 
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getQuizName() {
+        return quizName;
+    }
+
+    public void setQuizName(String quizName) {
+        this.quizName = quizName;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getTotalQuestions() {
+        return totalQuestions;
+    }
+
+    public void setTotalQuestions(int totalQuestions) {
+        this.totalQuestions = totalQuestions;
+    }
+
+    public int getCorrectQuestions() {
+        return correctQuestions;
+    }
+
+    public void setCorrectQuestions(int correctQuestions) {
+        this.correctQuestions = correctQuestions;
+    }
+
+    public float getGrade() {
+        return grade;
+    }
+
+    public void setGrade(float grade) {
+        this.grade = grade;
+    }
+
+    public String getFirstQuestion() {
+        return firstQuestion;
+    }
+
+    public void setFirstQuestion(String firstQuestion) {
+        this.firstQuestion = firstQuestion;
+    }
+
+    public boolean isExam() {
+        return isExam;
+    }
+
+    public void setExam(boolean exam) {
+        isExam = exam;
+    }
+
+    public boolean isTask() {
+        return isTask;
+    }
+
+    public void setTask(boolean task) {
+        isTask = task;
     }
 }
