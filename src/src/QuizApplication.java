96f65fa2b3c9b260dce09760dcf4e98d61196b1c
adi-tyapandey <adityapandey03@gmail.com>
2022-03-21 22:17:59 -0400

    initialized main

---
+++ src/src/QuizApplication.java
@@ -0,0 +1,16 @@
+package src.src;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+
+public class QuizApplication {
+    private static String welcomeMessage = "User Quiz Calculator:";
+    private static String menu = "Select one of the following options:\n" + "1. Import CSV file\n" + "2. Exit";
+    private static String error = "Please enter a valid choice.";
+    private static String exit = "Thank you for using the User Quiz Calculator.";
+    private static float quizDistribution;
+    private static float taskDistribution;
+    private static float finalDistribution;
+}
